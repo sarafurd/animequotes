014a6b9f278270fc999e182bd8e3be11ea2a3615
sara <sruthfd@csu.fullerton.edu>
2023-04-06 12:50:54 -0700

    commit

--- main.js
+++ main.js
@@ -7,22 +7,57 @@ function getFetch() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            let info = data[0];
-            console.log(info.anime);
+            console.log(data)
+                // let info = data[0];
+                // console.log(info.anime);
 
-            result.innerHTML = `
-            
-             <div class= "details">
-              <h2>${info.anime}</h2>
-              <h4>${info.character}</h4>
-             </div>
-             <div class="quote">${info.quote}</div>
-            `;
+            // result.innerHTML = `
+            // data.forEach(obj => {
+            //     <div class= "details">
+            //   <h2>${obj.anime}</h2>
+            //   <h4>${obj.character}</h4>
+            //  </div>
+            //  <div class="quote">${obj.quote}</div>
+            // })
+
+            // `;
+
+            // data.map(obj => {
+            //     result.innerHTML = `
+            //     <div class= "details">
+            //   <h2>${obj.anime}</h2>
+            //   <h4>${obj.character}</h4>
+            //  </div>
+            //  <div class="quote">${obj.quote}</div>
+            // `;
+            // })
+
+
+            var listItems = data.map(function(item) {
+                return `<div class="details">
+                        <h2>` + item.anime + `</h2>
+                        <h4>` + item.character + `</h4>
+                        </div>
+                        <div class="quote">
+                        ` + item.quote + `</div>
+                        <br>
+                        <br>`;
+
+
+            })
+
+            result.innerHTML = listItems
+
+
 
             document.querySelector('input').value = ''; // clear the input value
 
+
+
         }).catch(err => {
-            console.log(`error ${err}`)
+            console.log(`
+                            error $ { err }
+                            `)
         })
 
 }
