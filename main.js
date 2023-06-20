38071f1b6c977e8dce59afb0724ab3c3cc9f7fc5
sara <sruthfd@csu.fullerton.edu>
2023-06-19 21:40:40 -0700

    newest version

--- main.js
+++ main.js
@@ -2,26 +2,42 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch() {
     const choice = document.querySelector('input').value.toLowerCase();
-    const url = `https://animechan.vercel.app/api/quotes/anime?title=${choice}`;
+    const url = `http://animechan.melosh.space/random/anime?title=${choice}`;
 
     fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
 
-            var listItems = data.map(function(item) {
-                return `<div class="details">
-                        <h2>` + item.anime + `</h2>
-                        <h4>` + item.character + `</h4>
-                        </div>
-                        <div class="quote">
-                        ` + item.quote + `</div>
-                        <br>
-                        <br>`;
 
-            })
+            if (data.anime && data.character && data.quote) {
+                var html = `<div class="details">
+                              <h2>${data.anime}</h2>
+                              <h4>${data.character}</h4>
+                            </div>
+                            <div class="quote">
+                              ${data.quote}
+                            </div>
+                            <br>
+                            <br>`;
+                result.innerHTML = html;
+            } else {
+                result.innerHTML = 'No product found.';
+            }
 
-            result.innerHTML = listItems;
+            // var listItems = data.map(function(item) {
+            //     return `<div class="details">
+            //             <h2>` + item.anime + `</h2>
+            //             <h4>` + item.character + `</h4>
+            //             </div>
+            //             <div class="quote">
+            //             ` + item.quote + `</div>
+            //             <br>
+            //             <br>`;
+
+            // })
+
+            // result.innerHTML = listItems;
 
             document.querySelector('input').value = ''; // clear the input value
 
